cf7e712a342fb533c57c12ae34a6190834aaf34a
Nick DeRose <nderose7@gmail.com>
2023-11-20 17:46:11 -0500

    Change from 50 to 25 free credits in offer

--- components/SignUpOffer.vue
+++ components/SignUpOffer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="offer mt-20 mb-10 sm:text-center">
     <h2>Sign up for a free account</h2>
-    <p class="pt-1 mb-2">Get 50 free credits and save your favorite recipes.</p>
+    <p class="pt-1 mb-2">Get 25 free credits and save your favorite recipes.</p>
     <NuxtLink
       to="/sign-up"
       class="btn-primary inline-block my-2 btn-lg font-bold"
