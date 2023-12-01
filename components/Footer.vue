27637042cc3a86344a81c4d4a90bfbd34d7991b3
Nick DeRose <nderose7@gmail.com>
2023-12-01 13:15:53 -0500

    Add contact for support issues

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@
     >
     <span class="text-slate-400 dark:text-slate-600">&nbsp;|&nbsp;</span>
     <NuxtLink to="/contact" class="text-brand-500 font-medium text-base"
-      >Contact / Support</NuxtLink
+      >Contact</NuxtLink
     >
   </div>
 </template>
