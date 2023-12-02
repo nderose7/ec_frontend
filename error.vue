03fd2860d5ff6917867ed4e17868fe94256f7188
Nick DeRose <nderose7@gmail.com>
2023-12-02 10:04:05 -0500

    Add error page and category redirect wildcard

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dark:bg-midnight-800">
+    <div class="logo absolute pl-5 pt-2 text-2xl">
+      <Logo />
+    </div>
+    <div class="flex min-h-screen items-center text-center">
+      <div class="mx-auto">
+        <Icon name="game-icons:buoy" class="text-brand-500" size="5rem" />
+        <h1 class="mb-2 mt-5 text-5xl">{{ error.statusCode }} Error</h1>
+        <p>{{ error.statusMessage }}</p>
+
+        <NuxtLink to="/" class="btn-primary mt-6 inline-block"
+          ><Icon name="ion:arrow-back-circle-outline" class="icon-style" /> Back
+          to Homepage</NuxtLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const user = useStrapiUser();
+const props = defineProps({
+  error: Object,
+});
+</script>
